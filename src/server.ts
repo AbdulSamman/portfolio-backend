3c09c20b02b7193c3153dc1264d0d4c34eeb675e
AbdulSamman <sammanab@outlook.de>
2024-03-27 06:04:48 +0100

    test cors

--- src/server.ts
+++ src/server.ts
@@ -9,13 +9,14 @@ const port = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
+app.use(cors())
 // test
 app.get("/", (req: express.Request, res: express.Response) => {
   try {
